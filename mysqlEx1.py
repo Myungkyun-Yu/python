723b35ab7e33ac5c392e7b1c18afc3e4834a2d2e
Myungkyun-Yu <myungkyun.yu@gmail.com>
2020-01-18 14:40:09 +0900

    pymysql 라이브러리를 이용한 table 생성

---
+++ mysqlEx1.py
@@ -0,0 +1,27 @@
+import pymysql
+
+db = pymysql.connect(host='localhost', port=3306, user='root', passwd="9755", db='mkyu', charset='utf8')
+
+# Cursor 가져오기
+cursor = db.cursor()
+
+# SQL query 작성
+sql = """
+    create table fetish (
+        id int unsigned not null auto_increment,
+        name varchar(10) not null,
+        model_num varchar(20) not null,
+        model_type varchar(10) not null,
+        primary key(id)
+        );
+"""
+
+# query 실행하기
+cursor.execute(sql)
+
+# commit
+db.commit()
+
+# db 연결끊기
+db.close()
+
